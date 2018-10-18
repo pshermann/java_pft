b3a8b058507e11a3f2646a61e7b36c45bc9d737b
pshermann <pshermann@cinimex.com>
2018-10-18 18:05:34 +0300

    Добавил классы и конструкторы

--- sandbox/src/main/java/ru/stqa/pft/sandbox/FirstProgramm.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/FirstProgramm.java
@@ -4,12 +4,15 @@ public class FirstProgramm {
     public static void main(String[] args) {
         hello("QA");
         hello("Developer");
-        double l=5;
-        System.out.println("Площадь квадрата со стороной "+l+" = "+area(l));
 
+        Square s = new Square(25);
+
+        double l=5;
+        System.out.println("Площадь квадрата со стороной "+s.l+" = "+area(s));
+        Rectangle r = new Rectangle(22,24);
         double a=4;
         double b=6;
-        System.out.println("Площадь прямоугольника со сторонами "+a+" и "+b+" = "+area(a,b));
+        System.out.println("Площадь прямоугольника со сторонами "+r.a+" и "+r.b+" = "+area(r));
     }
 
     public static void hello(String wazza){
@@ -17,11 +20,11 @@ public class FirstProgramm {
     }
 
 
-    public static double area(double l){
-        return l*l;
+    public static double area(Square s){
+        return s.l*s.l;
     }
 
-    public static double area(double a, double b){
-        return a*b;
+    public static double area(Rectangle r){
+        return r.a*r.b;
     }
 }
---
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Square.java
@@ -0,0 +1,10 @@
+package ru.stqa.pft.sandbox;
+
+public class Square {
+
+    public double l;
+
+    public Square(double len){
+        this.l=len;
+    }
+}
