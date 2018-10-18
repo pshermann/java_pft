c1364e637e4f250f33333393ad87d16473516c56
pshermann <pshermann@cinimex.com>
2018-10-18 16:19:02 +0300

    Отформатировал код

--- sandbox/src/main/java/ru/stqa/pft/sandbox/FirstProgramm.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/FirstProgramm.java
@@ -1,7 +1,7 @@
 package ru.stqa.pft.sandbox;
 
 public class FirstProgramm {
-	public static void main(String[] args){
-	System.out.println("Hello, QA!");
-	}
+    public static void main(String[] args) {
+        System.out.println("Hello, QA!");
+    }
 }
